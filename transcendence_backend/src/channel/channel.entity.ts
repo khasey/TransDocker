4baab242935fece16fa1c8b706627fa28e16670f
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-07-19 18:21:44 +0200

    Add files via upload

---
+++ transcendence_backend/src/channel/channel.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity( { name: 'Channel' })
+export class Channel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  password?: string;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  owner: boolean
+
+  @Column()
+  isprivate: boolean
+
+
+  // Autres relations et propriétés...
+
+}
---
+++ transcendence_backend/src/channel/channel.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Channel } from './channel.entity';
+import { CreateChannelDto } from './channel.dto';
+import { User } from '../user/user.entity';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class ChannelService {
+	userRepository: any;
+	userService: any;
+  constructor(
+    @InjectRepository(Channel)
+    private channelRepository: Repository<Channel>,
+  ) {}
+
+
+  // ============ Créations de channels ============
+  async createChannel(createChannelDto: CreateChannelDto): Promise<Channel> {
+	const { name, password, userId, isprivate } = createChannelDto;
+
+	const channel = new Channel();
+	channel.name = name;
+	channel.password = password;
+	channel.userId = userId;
+	channel.owner = true;
+	channel.isprivate = isprivate;
+
+
+	console.log (channel.name);
+	console.log (channel.password);
+	console.log (channel.userId);
+	console.log (channel.owner);
+
+
+	return this.channelRepository.save(channel);
+  }
+
+  // ============ Créations de channels ============
+
+ // ============ Lister les channels ============
+  async getAllChannels(): Promise<Channel[]> {
+    return this.channelRepository.find();
+  }
+// ============ Lister les channels ============
+	async getLatestChannel(): Promise<Channel> {
+		return this.channelRepository
+	  	.createQueryBuilder("channel")
+	  	.orderBy("channel.id", "DESC") // Nous nous basons maintenant sur l'ID
+	  	.getOne(); // retourne le dernier canal créé
+  }
+}
